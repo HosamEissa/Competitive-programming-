d5dd89eae9606e8aceb59020505a3d8c04eac69c
HosamEissa <hosameissa.ds@gmail.com>
2017-06-11 05:39:39 +0200

    Create 10242.cpp

---
+++ UVA/10242.cpp
@@ -0,0 +1,68 @@
+/*
+Author: Hossam Eissa
+
+Idea:draw the two vectors
+
+*/
+#include <bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+#include<iomanip>
+#include<stdio.h>
+#include <map>
+#include<unordered_map>
+using namespace std;
+#define sf(a) scanf("%d",&a)
+#define sf2(a,b) scanf("%d %d",&a,&b)
+#define sf3(a,b,c) scanf("%d %d %d",&a,&b,&c)
+#define pf(a) printf("%d",a)
+#define pf2(a,b) printf("%d %d",a,b)
+#define pf3(a,b,c) printf("%d %d %d",a,b,c)
+#define sfl(a) scanf("%lld",&a)
+#define sfl2(a,b) scanf("%lld %lld",&a,&b)
+#define sfl3(a,b,c) scanf("%lld %lld %lld",&a,&b,&c)
+#define pfl(a) printf("%lld",a)
+#define pfl2(a,b) printf("%lld %lld",a,b)
+#define pfl3(a,b,c) printf("%lld %lld %lld",a,b,c)
+#define nl printf("\n")
+#define ll  long long
+#define se second
+#define fi first
+#define mk(a, b) make_pair(a, b)
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int, int>
+#define vvpi vector<vector<pii>>
+#define pli pair<ll, int>
+#define INF 2e15+5
+#define MAX (int)1e6 + 5
+#define FI freopen("input.txt", "r", stdin);
+#define FO freopen("o.txt", "w", stdout);
+#define in ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define edge pair<ll, pair<int, int>>
+#define CYA  exit(0);
+#define pi acos(-1);
+int dx[] = { 0, 1, 0, -1 };
+int dy[] = { 1, 0, -1, 0 }; // 4 Direction*/
+/*int dx[]={1,1,0,-1,-1,-1,0,1};
+int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction*/
+/*int dx[] = { 2,1,-1,-2,-2,-1,1,2 };
+int dy[] = { 1,2,2,1,-1,-2,-2,-1 };//Knight Direction*/
+const int N = 1e5+ 50;
+ll n, m, k;
+int main()
+{
+    cout <<fixed<<setprecision(3);
+   double x1,x2,x3,x4,y1,y2,y3,y4;
+   while(cin>>x1>>y1>>x2>>y2>>x3>>y3>>x4>>y4){
+    if(x1==x3&&y1==y3)swap(x2,x1),swap(y1,y2);
+    if(x1==x4&&y1==y4)swap(x2,x1),swap(y1,y2),swap(x3,x4),swap(y3,y4);
+    if(x2==x4&&y2==y4)swap(x3,x4),swap(y3,y4);
+   double tx1,ty1;
+   tx1=x1-x2;
+   ty1=y1-y2;
+   cout<<tx1+x4<<" "<<ty1+y4<<endl;
+   }
+
+}
