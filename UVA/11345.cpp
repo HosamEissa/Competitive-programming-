76c71fb9b4a7027fbfe9781e9788716d9269e2ad
HosamEissa <hosameissa.ds@gmail.com>
2017-06-11 05:59:45 +0200

    Create 11345.cpp

---
+++ UVA/11345.cpp
@@ -0,0 +1,78 @@
+/*
+Author: Hossam Eissa
+
+Idea:
+
+*/
+#include <bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+#include<iomanip>
+#include<stdio.h>
+#include <map>
+#include<unordered_map>
+using namespace std;
+#define sf(a) scanf("%d",&a)
+#define sf2(a,b) scanf("%d %d",&a,&b)
+#define sf3(a,b,c) scanf("%d %d %d",&a,&b,&c)
+#define pf(a) printf("%d",a)
+#define pf2(a,b) printf("%d %d",a,b)
+#define pf3(a,b,c) printf("%d %d %d",a,b,c)
+#define sfl(a) scanf("%lld",&a)
+#define sfl2(a,b) scanf("%lld %lld",&a,&b)
+#define sfl3(a,b,c) scanf("%lld %lld %lld",&a,&b,&c)
+#define pfl(a) printf("%lld",a)
+#define pfl2(a,b) printf("%lld %lld",a,b)
+#define pfl3(a,b,c) printf("%lld %lld %lld",a,b,c)
+#define nl printf("\n")
+#define ll  long long
+#define se second
+#define fi first
+#define mk(a, b) make_pair(a, b)
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int, int>
+#define vvpi vector<vector<pii>>
+#define pli pair<ll, int>
+#define INF 2e15+5
+#define MAX (int)1e6 + 5
+#define FI freopen("input.txt", "r", stdin);
+#define FO freopen("o.txt", "w", stdout);
+#define in ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define edge pair<ll, pair<int, int>>
+#define CYA  exit(0);
+#define pi acos(-1);
+int dx[] = { 0, 1, 0, -1 };
+int dy[] = { 1, 0, -1, 0 }; // 4 Direction*/
+/*int dx[]={1,1,0,-1,-1,-1,0,1};
+int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction*/
+/*int dx[] = { 2,1,-1,-2,-2,-1,1,2 };
+int dy[] = { 1,2,2,1,-1,-2,-2,-1 };//Knight Direction*/
+const int N = 1e5+ 50;
+ll n, m, k;
+int main()
+{
+  //  FO;
+    int t;
+    cin>>t;
+    for(int tc=0;tc<t;tc++){
+        cin>>n;
+        int x1,x2,y1,y2;
+        x1=y1=-1e5,x2=y2=1e5;
+        for(int i=0;i<n;i++){
+            int a1,a2,b1,b2;
+            cin>>a1>>b1>>a2>>b2;
+            x1=max(x1,a1);
+            y1=max(y1,b1);
+            x2=min(x2,a2);
+            y2=min(y2,b2);
+        }
+
+        ll area=(x2-x1)*(y2-y1);
+        if(x2-x1<0||y2-y1<0)area=0;
+     //   cout<<x1<<" "<<y1<<" "<<x2<<" "<<y2<<endl;
+        printf("Case %d: %lld\n",tc+1,area);
+    }
+
+}
