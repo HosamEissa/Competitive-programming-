059598fbf8e711243d347d1a180d6e831c5a9ce6
HosamEissa <hosameissa.ds@gmail.com>
2017-06-13 01:44:48 +0200

    Create 2015-Hist-A.cpp

---
+++ Codeforces/2015-Hist-A.cpp
@@ -0,0 +1,98 @@
+/*
+Author: Hossam Eissa
+
+Idea:
+
+*/
+#include <bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+#include<iomanip>
+#include<stdio.h>
+#include <map>
+#include<unordered_map>
+#include<complex>
+using namespace std;
+typedef complex<double> point;
+#define X real()
+#define Y imag()
+#define angle(a)                (atan2((a).imag(), (a).real()))
+#define vec(a,b)                ((b)-(a))
+#define same(p1,p2)             (dp(vec(p1,p2),vec(p1,p2)) < EPS)
+#define dp(a,b)                 ( (conj(a)*(b)).real() )	// a*b cos(T), if zero -> prep
+#define cp(a,b)                 ( (conj(a)*(b)).imag() )	// a*b sin(T), if zero -> parllel
+#define length(a)               (hypot((a).imag(), (a).real()))
+#define normalize(a)            (a)/length(a)
+//#define polar(r,ang)            ((r)*exp(point(0,ang)))  ==> Already added in c++11
+#define rotateO(p,ang)          ((p)*exp(point(0,ang)))
+#define rotateA(p,ang,about)  (rotateO(vec(about,p),ang)+about)
+#define reflectO(v,m)  (conj((v)/(m))*(m))
+
+
+#define sf(a) scanf("%d",&a)
+#define sf2(a,b) scanf("%d %d",&a,&b)
+#define sf3(a,b,c) scanf("%d %d %d",&a,&b,&c)
+#define pf(a) printf("%d",a)
+#define pf2(a,b) printf("%d %d",a,b)
+#define pf3(a,b,c) printf("%d %d %d",a,b,c)
+#define sfl(a) scanf("%lld",&a)
+#define sfl2(a,b) scanf("%lld %lld",&a,&b)
+#define sfl3(a,b,c) scanf("%lld %lld %lld",&a,&b,&c)
+#define pfl(a) printf("%lld",a)
+#define pfl2(a,b) printf("%lld %lld",a,b)
+#define pfl3(a,b,c) printf("%lld %lld %lld",a,b,c)
+#define nl printf("\n")
+#define ll  long long
+#define FI freopen("input.txt", "r", stdin);
+#define FO freopen("o.txt", "w", stdout);
+#define in ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define edge pair<ll, pair<int, int>>
+#define CYA  exit(0);
+#define pi acos(-1);
+int dx[] = { 0, 1, 0, -1 };
+int dy[] = { 1, 0, -1, 0 }; // 4 Direction*/
+/*int dx[]={1,1,0,-1,-1,-1,0,1};
+int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction*/
+/*int dx[] = { 2,1,-1,-2,-2,-1,1,2 };
+int dy[] = { 1,2,2,1,-1,-2,-2,-1 };//Knight Direction*/
+const int N = 1e5+ 50;
+const double EPS=1e-8;
+const double INF=2e15+5;
+ll n, m, k;
+int arr[N],sz[N];
+int f(int a){
+return arr[a]= arr[a]==a?a:f(arr[a]);
+}
+void connect(int a,int b){
+a=f(a);
+b=f(b);
+if(a==b)return ;
+if(sz[a]<sz[b]){
+    arr[a]=b;
+    sz[b]+=sz[a];
+}
+else {
+    arr[b]=a;
+    sz[a]+=sz[b];
+}
+
+}
+int main()
+{
+   // FO;
+        in;
+    int t;
+    cin>>t;
+    while(t--){
+        int h1,m1,s1,h2,m2,s2;
+        cin>>h1>>m1>>s1>>h2>>m2>>s2;
+        int total1=h1*60*60+m1*60+s1;
+        int total2=h2*60*60+m2*60+s2;
+        if(total1<total2){
+            cout<<"Player1"<<endl;
+        }
+        else if(total1>total2)cout<<"Player2"<<endl;
+        else cout<<"Tie"<<endl;
+    }
+
+}
