c147cf83a4edac1a26d9d703f54c853ba02e8bf4
HosamEissa <hosameissa.ds@gmail.com>
2017-06-13 01:47:57 +0200

    Create 2015-Hist-D.cpp

---
+++ Codeforces/2015-Hist-D.cpp
@@ -0,0 +1,94 @@
+/*
+Author: Hossam Eissa
+
+Idea:
+
+*/
+#include <bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+#include<iomanip>
+#include<stdio.h>
+#include <map>
+#include<unordered_map>
+#include<complex>
+using namespace std;
+typedef complex<double> point;
+#define X real()
+#define Y imag()
+#define angle(a)                (atan2((a).imag(), (a).real()))
+#define vec(a,b)                ((b)-(a))
+#define same(p1,p2)             (dp(vec(p1,p2),vec(p1,p2)) < EPS)
+#define dp(a,b)                 ( (conj(a)*(b)).real() )	// a*b cos(T), if zero -> prep
+#define cp(a,b)                 ( (conj(a)*(b)).imag() )	// a*b sin(T), if zero -> parllel
+#define length(a)               (hypot((a).imag(), (a).real()))
+#define normalize(a)            (a)/length(a)
+//#define polar(r,ang)            ((r)*exp(point(0,ang)))  ==> Already added in c++11
+#define rotateO(p,ang)          ((p)*exp(point(0,ang)))
+#define rotateA(p,ang,about)  (rotateO(vec(about,p),ang)+about)
+#define reflectO(v,m)  (conj((v)/(m))*(m))
+
+
+#define sf(a) scanf("%d",&a)
+#define sf2(a,b) scanf("%d %d",&a,&b)
+#define sf3(a,b,c) scanf("%d %d %d",&a,&b,&c)
+#define pf(a) printf("%d",a)
+#define pf2(a,b) printf("%d %d",a,b)
+#define pf3(a,b,c) printf("%d %d %d",a,b,c)
+#define sfl(a) scanf("%lld",&a)
+#define sfl2(a,b) scanf("%lld %lld",&a,&b)
+#define sfl3(a,b,c) scanf("%lld %lld %lld",&a,&b,&c)
+#define pfl(a) printf("%lld",a)
+#define pfl2(a,b) printf("%lld %lld",a,b)
+#define pfl3(a,b,c) printf("%lld %lld %lld",a,b,c)
+#define nl printf("\n")
+#define ll  long long
+#define FI freopen("input.txt", "r", stdin);
+#define FO freopen("o.txt", "w", stdout);
+#define in ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define edge pair<ll, pair<int, int>>
+#define CYA  exit(0);
+#define pi acos(-1);
+int dx[] = { 0, 1, 0, -1 };
+int dy[] = { 1, 0, -1, 0 }; // 4 Direction*/
+/*int dx[]={1,1,0,-1,-1,-1,0,1};
+int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction*/
+/*int dx[] = { 2,1,-1,-2,-2,-1,1,2 };
+int dy[] = { 1,2,2,1,-1,-2,-2,-1 };//Knight Direction*/
+const int N = 2e2+ 50;
+const int MOD = 1e9+ 7;
+const double EPS=1e-8;
+const double INF=2e15+5;
+ll n, m,k,d;
+int arr[N];
+ll dp[205][205][205];
+ll solve(int idx,int cnt,int cntd){
+if(cnt==m){
+if(cntd>=k)return 1;
+else return 0;
+}
+if(idx==n)return 0;
+
+ll &ans=dp[idx][cnt][cntd];
+if(ans!=-1)return ans;
+ans=0;
+ans=(ans+solve(idx+1,cnt,cntd))%MOD;
+if(arr[idx]>=d)ans=(ans+solve(idx+1,cnt+1,cntd+1))%MOD;
+else ans=(ans+solve(idx+1,cnt+1,cntd))%MOD;
+
+return ans=(ans%MOD);
+}
+int main()
+{
+    // FO;
+    in;
+    int t=1;
+    cin>>t;
+    while(t--){
+        cin>>n>>m>>k>>d;
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        memset(dp,-1,sizeof dp);
+        cout<<solve(0,0,0)<<endl;;
+    }
+}
