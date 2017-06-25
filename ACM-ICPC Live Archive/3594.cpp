043f799f8d543973cfabb02db07cf68cd1a9c2d4
HosamEissa <hosameissa.ds@gmail.com>
2017-06-25 10:56:30 +0200

    Create 3594.cpp

---
+++ ACM-ICPC Live Archive/3594.cpp
@@ -0,0 +1,63 @@
+/*
+Author: Hossam Eissa
+Idea:
+*/
+#include <bits/stdc++.h>
+#include<stack>
+#include <stdio.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<iomanip>
+#include<stdio.h>
+#include <map>
+#include<unordered_map>
+#include<complex>
+using namespace std;
+#define sf(a) scanf("%d",&a)
+#define sf2(a,b) scanf("%d %d",&a,&b)
+#define sf3(a,b,c) scanf("%d %d %d",&a,&b,&c)
+#define pf(a) printf("%d",a)
+#define pf2(a,b) printf("%d %d",a,b)
+#define pf3(a,b,c) printf("%d %d %d",a,b,c)
+#define sfl(a) scanf("%lld",&a)
+#define sfl2(a,b) scanf("%lld %lld",&a,&b)
+#define sfl3(a,b,c) scanf("%lld %lld %lld",&a,&b,&c)
+#define pfl(a) printf("%lld",a)
+#define pfl2(a,b) printf("%lld %lld",a,b)
+#define pfl3(a,b,c) printf("%lld %lld %lld",a,b,c)
+#define nl printf("\n")
+#define ll  long long
+#define FI freopen("interesting.in", "r", stdin);
+#define FO freopen("o.txt", "w", stdout);
+#define in ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define edge pair<ll, pair<int, int>>
+#define CYA  exit(0);
+/*int dx[] = { 0, 1, 0, -1 };
+int dy[] = { 1, 0, -1, 0 }; // 4 Direction*/
+int dx[]={1,1,0,-1,-1,-1,0,1};
+int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction*/
+/*int dx[] = { 2,1,-1,-2,-2,-1,1,2 };
+int dy[] = { 1,2,2,1,-1,-2,-2,-1 };//Knight Direction*/
+const int N = 1e3 + 5;
+const double EPS = 1e-10;
+const ll INF = 1e18;
+const double PI = acos(-1);
+ll n, m, k;
+int dcmp(double a, double b)
+{
+    return fabs(a - b) <= EPS ? 0 : a<b ? -1 : 1;
+}
+int main()
+{
+    in;
+    string s;
+    while(getline(cin,s)&&s[0]!='#'){
+            ll total=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]==' ')continue;
+            total+=(s[i]-'A'+1)*(i+1);
+        }
+        cout<<total<<endl;
+    }
+}
